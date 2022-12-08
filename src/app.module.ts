6616273cafe30b74a39dc6a46c895d37f99138d2
Evgeny Popov <eugene.eva.20@gamil.com>
2022-12-09 00:47:14 +0600

    Deploy testing.

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,16 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductModule } from './products/product.module';
 import { UserModule } from './users/users.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
     UserModule,
     ProductModule,
-    MongooseModule.forRoot('mongodb://localhost/nest'),
+    ConfigModule.forRoot({
+      envFilePath: ['.env.development.local'],
+    }),
+    MongooseModule.forRoot(process.env.MONGO_URI, { useFindAndModify: false }),
   ],
   controllers: [AppController],
   providers: [AppService],
